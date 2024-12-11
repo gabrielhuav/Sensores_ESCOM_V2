ee42d147ce459773814a624aa5a9567299499dfc
AMorTor <amoralest020906@hotmail.com>
2024-12-10 22:03:24 -0600

    Feat: adding refactoring activity / vertical and horizontal constraints

--- app/src/main/java/ovh/gabrielhuav/sensores_escom_v2/presentation/components/GameplayActivity.kt
+++ app/src/main/java/ovh/gabrielhuav/sensores_escom_v2/presentation/components/GameplayActivity.kt
@@ -12,7 +12,7 @@ class GameplayActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_gameplay)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_layout)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
