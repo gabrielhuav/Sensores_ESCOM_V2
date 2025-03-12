cdd8ef300d54f3e84ce3735ffcf732d5c0354a22
Eduardo Islas <126023020+GojiLalo@users.noreply.github.com>
2025-03-12 16:34:26 -0600

    Matriz estacionamiento
    Matriz mapa estacionamiento empezada

--- app/src/main/java/ovh/gabrielhuav/sensores_escom_v2/presentation/components/ipn.zacatenco.escom.mainBuilding/ServerConnectionManager.kt
+++ app/src/main/java/ovh/gabrielhuav/sensores_escom_v2/presentation/components/ipn.zacatenco.escom.mainBuilding/ServerConnectionManager.kt
@@ -12,7 +12,7 @@ class ServerConnectionManager(
     val onlineServerManager: OnlineServerManager
 ) {
     //private val serverUrl = "ws://192.168.1.20:3000"
-    private val serverUrl = "ws://192.168.100.2:3000"
+    private val serverUrl = "ws://172.100.91.226:3000"
     private var isConnecting = false
     private val mainHandler = Handler(Looper.getMainLooper())
 
