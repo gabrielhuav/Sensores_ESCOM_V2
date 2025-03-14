bd3f2ebf0305bfe664cf15c3f49a7a25f8c91fcf
0ambar <abrahamr677@gmail.com>
2025-03-13 19:37:01 -0600

    app finished

---
+++ app/src/main/java/ovh/gabrielhuav/sensores_escom_v2/presentation/components/ipn/zacatenco/EscomPlaces.kt
@@ -0,0 +1,21 @@
+package ovh.gabrielhuav.sensores_escom_v2.presentation.components.ipn.zacatenco
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import ovh.gabrielhuav.sensores_escom_v2.R
+
+class EscomPlaces : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_escom_places)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
