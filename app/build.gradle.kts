6fbaf4d809ad83ef870d157f2f1406350e176b36
Gabriel Hurtado <71236850+gabrielhuav@users.noreply.github.com>
2025-12-08 17:44:12 -0600

    QA changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,4 +61,5 @@ dependencies {
     implementation("org.osmdroid:osmdroid-android:6.1.17")
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
     implementation("androidx.cardview:cardview:1.0.0")
+    implementation("com.github.MKergall:osmbonuspack:6.9.0")
 }
